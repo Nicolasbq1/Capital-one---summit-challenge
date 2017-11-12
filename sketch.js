6c86191529b4e45fbd38d24d95a7d0ce468b9927
Nicolas Buitrago <nb486@cornell.edu>
2017-11-12 02:09:16 -0500

    not fully parsing yet

--- sketch.js
+++ sketch.js
@@ -1,8 +1,37 @@
+var elem;
+var w;
 function setup() {
-  createCanvas(600,600);
-  background(130);
+  var canvas = createCanvas(600,600);
+  canvas.parent('sketch_holder');
+  elem = (document.compatMode === "CSS1Compat") ?
+    document.documentElement :
+    document.body;
+  w = elem.clientWidth;
+  resizeCanvas(w,.5*  windowHeight);
+  background('#00A699');
+  parseData();
 }
 
 function draw() {
+
   // put drawing code here
+}
+
+function windowResized() {
+  w = elem.clientWidth;
+  resizeCanvas(w,.5*  windowHeight);
+}
+
+function parseData(){
+  Papa.parse("longandprice.csv", {
+  	complete: function(results) {
+  		console.log("Finished:", results.data);
+      getData(results.data)
+  	}
+  });
+}
+
+function getData(data) {
+
+
 }
