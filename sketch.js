07aa63033aa11c74b3beafb293a5570fe3786175
Shadman Khan <skhanteak13@gmail.com>
2017-11-13 02:14:28 -0500

    finished graph beginning other states

--- sketch.js
+++ sketch.js
@@ -1,5 +1,12 @@
 var elem;
 var w;
+var h;
+var neighavg;
+var loadbar = new load();
+var load = false;
+var bararr;
+var neighbar;
+var state = "graph";
 function setup() {
   var canvas = createCanvas(600,600);
   canvas.parent('sketch_holder');
@@ -7,31 +14,230 @@ function setup() {
     document.documentElement :
     document.body;
   w = elem.clientWidth;
-  resizeCanvas(w,.5*  windowHeight);
+  h = .6*windowHeight;
+  resizeCanvas(w,h);
   background('#00A699');
   parseData();
+  bararr = [];
+  neighbar = new Map();
 }
 
 function draw() {
+  background(255);
+  if(neighavg == undefined){
+    loadbar.render();
+    loadbar.update();
+  }
+  else if(!load){
+    load = true;
+    var i = 0;
+    for(var [key,value] of neighavg){
+      bararr.push(new bar(i,key,value));
+      i++;
+    }
+  }
+  else{
+    push();
+    stroke("#fc642d");
+    strokeWeight(4);
+    line(.08*w,.9*h,(1-.08)*w,.9*h);
+    line(.08*w,.9*h,.08*w,.2*h);
+    var lh = .9*h-50;
+    while(lh > .2*h){
+      line(.08*w-5,lh,.08*w+5,lh);
+      push();
+      textSize(10);
+      noStroke();
+      var lmark = -1*(lh-.9*h);
+      text("$"+lmark.toString(),.08*w-30,lh+3);
+      pop();
+      lh -= 50;
+    }
+    for(var i = 0;i < 37;i++){
+      bararr[i].render();
+    }
+    info(geti());
+    pop();
+  }
+}
 
-  // put drawing code here
+function geti(){
+  var i = (mouseX-3-.08*w)/((1/37)*(1-.16)*w);
+  return Math.floor(i);
+}
+
+function info(i){
+  if(i >= 0 && i <= 36){
+    if(mouseY >= bararr[i].y-bararr[i].height && mouseY <=bararr[i].y){
+      if(mouseX <= w-140 && mouseY >= 100){
+        push();
+        stroke(0);
+        fill(255);
+        beginShape();
+        vertex(mouseX,mouseY);
+        vertex(mouseX,mouseY-100);
+        vertex(mouseX + 140, mouseY -100);
+        vertex(mouseX + 140, mouseY -20);
+        vertex(mouseX + 20, mouseY -20);
+        endShape(CLOSE);
+        fill(0);
+        strokeWeight(1);
+        text(bararr[i].neigh,mouseX + 10,mouseY-80);
+        text("$"+bararr[i].avgp,mouseX + 10,mouseY-50);
+        pop();
+      }
+      else if(mouseX <= w -140){
+        push();
+        stroke(0);
+        fill(255);
+        beginShape();
+        vertex(mouseX,mouseY);
+        vertex(mouseX,mouseY+100);
+        vertex(mouseX + 140, mouseY +100);
+        vertex(mouseX + 140, mouseY +20);
+        vertex(mouseX + 20, mouseY +20);
+        endShape(CLOSE);
+        fill(0);
+        strokeWeight(1);
+        text(bararr[i].neigh,mouseX + 10,mouseY+50);
+        text("$"+bararr[i].avgp,mouseX + 10,mouseY + 80);
+        pop();
+      }
+      else if(mouseY >= 100){
+        push();
+        stroke(0);
+        fill(255);
+        beginShape();
+        vertex(mouseX,mouseY);
+        vertex(mouseX,mouseY-100);
+        vertex(mouseX - 140, mouseY -100);
+        vertex(mouseX - 140, mouseY -20);
+        vertex(mouseX - 20, mouseY -20);
+        endShape(CLOSE);
+        fill(0);
+        strokeWeight(1);
+        text(bararr[i].neigh,mouseX + 10-140,mouseY-80);
+        text("$"+bararr[i].avgp,mouseX + 10-140,mouseY-50);
+        pop();
+      }
+      else{
+        push();
+        stroke(0);
+        fill(255);
+        beginShape();
+        vertex(mouseX,mouseY);
+        vertex(mouseX,mouseY+100);
+        vertex(mouseX - 140, mouseY +100);
+        vertex(mouseX - 140, mouseY +20);
+        vertex(mouseX - 20, mouseY +20);
+        endShape(CLOSE);
+        fill(0);
+        strokeWeight(1);
+        text(bararr[i].neigh,mouseX + 10-140,mouseY+50);
+        text("$"+bararr[i].avgp,mouseX + 10-140,mouseY+80);
+        pop();
+      }
+    }
+    else{
+      console.log(false);
+    }
+  }
+}
+
+function bar(i,neigh,avgp){
+  this.neigh = neigh;
+  this.avgp = avgp;
+  this.x = .08*w+(1/37)*(1-.16)*w*i+3;
+  this.y = .9*h-2;
+  this.height = this.avgp;
+  this.width = (1/37)*(1-.16)*w-2;
+  this.render = function(){
+    this.x = .08*w+(1/37)*(1-.16)*w*i+3;
+    this.y = .9*h-2;
+    this.width = (1/37)*(1-.16)*w-2;
+    push();
+    noStroke();
+    fill('#00A699');
+    rect(this.x,this.y,this.width,-this.height);
+    pop();
+  }
+}
+
+
+function load(){
+  this.pi = 3.14159265358979323;
+  this.theta = 1.5*this.pi;
+  this.update = function(){
+    this.theta += .1;
+  }
+  this.render = function(){
+    push();
+    stroke("#fc642d");
+    fill("#fc642d");
+    arc(.5*w, .5*h, 80, 80, 1.5*PI, this.theta, PIE);
+    strokeWeight(1);
+    text("Loading",.5*w-20,.5*h+60);
+    pop();
+  }
 }
 
 function windowResized() {
   w = elem.clientWidth;
-  resizeCanvas(w,.5*  windowHeight);
+  h = .6*windowHeight;
+  resizeCanvas(w,h);
 }
 
 function parseData(){
   Papa.parse("longandprice.csv", {
+    download: true,
   	complete: function(results) {
-  		console.log("Finished:", results.data);
-      getData(results.data)
+      initializeDict(simplifyData(results.data));
   	}
   });
 }
 
-function getData(data) {
+function initializeDict(parsedata){
+  Papa.parse("neighbourhoods.csv", {
+    download: true,
+  	complete: function(results) {
+      getData(parsedata,rectifyData(results.data));
+  	}
+  });
+}
 
+function simplifyData(data){
+  var array = [];
+  data.forEach(function(element) {
+    if(element.length > 3){
+      array.push(element);
+        }
+  });
+  array.forEach(function(element){
+    element[0] = element[0].trim();
+  });
+  return array
+}
 
+function rectifyData(data){
+  var dict = new Map();
+  data.forEach(function(element,index){
+    if(index > 0 && element.length > 1){
+      dict.set(element[1],0);
+    }
+  });
+  return dict
+}
+
+function getData(data,dict) {
+  var averages = new Map(dict);
+  var accum_div = new Map(dict);
+  var final = new Map(dict);
+  data.forEach(function(element,index){
+    averages.set(element[0],averages.get(element[0]) + parseInt(element[3].slice(1)));
+    accum_div.set(element[0],accum_div.get(element[0])+1);
+  });
+  final.forEach(function(value,key){
+    final.set(key,averages.get(key)/accum_div.get(key));
+  });
+  neighavg = new Map(final);
 }
